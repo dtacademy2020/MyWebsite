e7333f084b3328c1c5d5025e11f8b9cf7dc4186f
Duotech <duotechinstructor@gmail.com>
2020-06-13 19:06:27 -0400

    changed 2 files

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ var button = document.getElementById('theButton');
 
 //add event listener
 button.addEventListener('click', function(event) {
-  alert("You are awesome!");
+  alert("You are awful!");
 });
